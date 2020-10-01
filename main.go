56f6f3522e8e4534a2a4ce183ac2bfe78447dd4a
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-01 18:57:22 +0000

    Correcion de valores

--- main.go
+++ main.go
@@ -10,16 +10,25 @@ func main() {
 	fmt.Scanln(&lim)
 	i := 0
 	aux := 1
+	flag := 0
 
-	for i < lim {
-		for l := 1; l <= i+1; l++ {
-			aux *= l
+	for i <= lim {
+		for l := 0; l <= i; l++ {
+			flag = 0
+			if l == 0{
+				flag = 1
+			} else{
+				aux *= l
+			}
 		}
-		if aux != 0 {
+		if flag != 1 {
 			e += (1.0 / float64(aux))
 			aux = 1
+			fmt.Println("Actual:",e)
+		} else {
+			fmt.Println("Actual: 1")
 		}
-		fmt.Println("Actual:",e)
+		
 		i++
 	}
 
