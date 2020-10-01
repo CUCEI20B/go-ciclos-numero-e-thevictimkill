662de8aad1514c41e2e77e3484e4b6bec64a5053
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-01 18:18:07 +0000

    Codigo completo

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var lim int
+ 	//int var e
+	e := 0.0
+	fmt.Scanln(&lim)
+	i := 0
+	aux := 1
+
+	for i <= lim {
+		for l := 1; l <= i+1; l++ {
+			aux *= l
+		}
+		if aux != 0 {
+			e += (1.0 / float64(aux))
+			aux = 1
+		}
+		fmt.Println(e)
+		i++
+	}
+
 }
