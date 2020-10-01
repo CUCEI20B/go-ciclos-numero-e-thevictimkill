7d58841511bc86b2999b5842ed9d845e04324855
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-01 19:11:31 +0000

    Correcion de logica

--- main.go
+++ main.go
@@ -10,23 +10,19 @@ func main() {
 	fmt.Scanln(&lim)
 	i := 0
 	aux := 1
-	flag := 0
 
 	for i <= lim {
 		for l := 0; l <= i; l++ {
-			flag = 0
 			if l == 0{
-				flag = 1
+				aux = 1
 			} else{
 				aux *= l
 			}
 		}
-		if flag != 1 {
+		if aux != 0 {
 			e += (1.0 / float64(aux))
 			aux = 1
-			fmt.Println("Actual:",e)
-		} else {
-			fmt.Println("Actual: 1")
+			fmt.Println(e)
 		}
 		
 		i++
